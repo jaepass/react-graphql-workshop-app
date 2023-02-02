091cc112ac1b1504e9da75d672323f04d8c6810a
jaepass <jaeriahpasseri@gmail.com>
2023-02-01 22:49:18 -0800

    Edit files back to default

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/local',
-  cache: new InMemoryCache(),
-})
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <GlobalStyles />
     <Pages />
-  </ApolloProvider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
--- client/src/pages/initiatives.js
+++ client/src/pages/initiatives.js
@@ -1,45 +1,11 @@
 import React from 'react';
 import { Layout } from '../components';
-import { useQuery, gql } from '@apollo/client';
-import InitiativeCard from '../containers/initiative-card';
-import QueryResult from '../components/query-result';
-
-export const INITIATIVES = gql`
-  query initiativesForMain {
-    initiativesForMain {
-      id
-      name
-      organization {
-        name
-        image
-        id
-      }
-      image
-      description
-    }
-  }
-`;
 
 /**
- * Initiatives Page is the Climate Initiatives home page.
- * We display a grid of initiatives fetched with useQuery with the INITIATIVES query
+ * Initiatives Page
  */
 const Initiatives = () => {
-  const { loading, error, data } = useQuery(INITIATIVES);
-
-  if (loading) return 'Loading...';
-
-  if (error) return `Error ${error.message}`;
-
-  return (
-    <Layout grid>
-      <QueryResult error={error} loading={loading} data={data}>
-        { data?.initiativesForMain?.map((initiative) => (
-          <InitiativeCard key={initiative.id} initiative={initiative} />
-        ))}
-      </QueryResult>
-    </Layout>
-  )
+  return <Layout grid> </Layout>;
 };
 
 export default Initiatives;
